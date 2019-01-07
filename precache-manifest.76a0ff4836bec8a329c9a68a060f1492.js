1557a57b539b3736a70a7de84a0af926f04de6b7
Joshua <jrobson68@hotmail.co.uk>
2019-01-07 09:33:29 +0000

    Updates

---
+++ precache-manifest.76a0ff4836bec8a329c9a68a060f1492.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ravenous/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9f839d09ce85f2d9931f",
+    "url": "/ravenous/static/js/runtime~main.9f839d09.js"
+  },
+  {
+    "revision": "35516c461cf5ad6ef836",
+    "url": "/ravenous/static/js/main.35516c46.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/ravenous/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "35516c461cf5ad6ef836",
+    "url": "/ravenous/static/css/main.58e3bfc9.chunk.css"
+  },
+  {
+    "revision": "896e60094ea8e72488664591c395cc8c",
+    "url": "/ravenous/index.html"
+  }
+];

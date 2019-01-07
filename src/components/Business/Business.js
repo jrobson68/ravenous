ecb5ad0ebafefc9c636b674e093edfa2402448f9
Joshua <jrobson68@hotmail.co.uk>
2019-01-07 11:22:55 +0000

    Fixed React import statement

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -1,4 +1,4 @@
-import react from 'react';
+import React from 'react';
 import './Business.css';
 
 const business = {
--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -1,6 +1,6 @@
-import react from 'react';
+import React from 'react';
 import './BusinessList.css';
-import 'Business' from '../Business/Business';
+import Business from '../Business/Business';
 
 class BusinessList extends React.Component {
 	render() {
@@ -16,3 +16,5 @@ class BusinessList extends React.Component {
 		)
 	}
 }
+
+export default BusinessList;

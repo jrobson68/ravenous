6e32bd4ed3e653e23185bc14c66c73b50e7a58b3
Joshua <jrobson68@hotmail.co.uk>
2019-01-07 11:27:00 +0000

    Updates

---
+++ precache-manifest.72c494d348216e847498fb39590e2dbf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ravenous/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9f839d09ce85f2d9931f",
+    "url": "/ravenous/static/js/runtime~main.9f839d09.js"
+  },
+  {
+    "revision": "0a4f0df86a80bc6094a5",
+    "url": "/ravenous/static/js/main.0a4f0df8.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/ravenous/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "0a4f0df86a80bc6094a5",
+    "url": "/ravenous/static/css/main.5b96c78a.chunk.css"
+  },
+  {
+    "revision": "cb64211d0cb8d18c14bdaf2d58f88fe3",
+    "url": "/ravenous/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ravenous/precache-manifest.6912dbb1b604a8a771382b369914f4bb.js"
+  "/ravenous/precache-manifest.72c494d348216e847498fb39590e2dbf.js"
 );
 
 workbox.clientsClaim();
